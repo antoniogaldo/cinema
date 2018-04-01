7f9983fa49ef5534866b5fcb1059dcd5de05d4e8
antoniogaldo <antoniohendrix@hotmail.it>
2018-04-02 01:11:36 +0200

    form cinema

--- src/Controller/Cinema/CinemaController.php
+++ src/Controller/Cinema/CinemaController.php
@@ -6,6 +6,11 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Cinema;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CinemaController extends Controller
 {
@@ -20,10 +25,43 @@ class CinemaController extends Controller
     /**
      * @Route("/cinema", name="cinema")
      */
-    public function index()
+    public function index(Request $request)
     {
+       $cinema = new Cinema();
+
+       $form = $this->createFormBuilder($cinema)
+           ->add('nome', TextType::class)
+           ->add('citta', TextType::class)
+           ->add('sale', IntegerType::class)
+           ->add('save', SubmitType::class, array('label' => 'Create Task'))
+           ->getForm();
+           if ($request->isMethod('POST')) {
+                       // handle the first form
+                       $form->handleRequest($request);
+                       // set field in to db //
+                       $nome = $form['nome']->getData();
+                       $citta = $form['citta']->getData();
+                       $sale = $form['sale']->getData();
+
+                       $cinema->setNome($nome);
+                       $cinema->setCitta($citta);
+                       $cinema->setSale($sale);
+
+                       $sn = $this->getDoctrine()->getManager();
+                       // control form //
+                         if($form->isSubmitted() &&  $form->isValid()){
+                           // insert in to db //
+                           $sn -> persist($cinema);
+                           $sn -> flush();
+                           // alert success //
+                           $request->getSession()
+                           ->getFlashBag()
+                           ->add('success', 'Grazie per esserti iscritto, riceverai un email di conferma');
+                }
+              }
         return $this->render('cinema/index.html.twig', [
             'controller_name' => 'CinemaController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Entity/Cinema.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\CinemaRepository")
+ */
+class Cinema
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nome;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $citta;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $sale;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getNome(): ?string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome): self
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    public function getCitta(): ?string
+    {
+        return $this->citta;
+    }
+
+    public function setCitta(string $citta): self
+    {
+        $this->citta = $citta;
+
+        return $this;
+    }
+
+    public function getSale(): ?int
+    {
+        return $this->sale;
+    }
+
+    public function setSale(int $sale): self
+    {
+        $this->sale = $sale;
+
+        return $this;
+    }
+}
